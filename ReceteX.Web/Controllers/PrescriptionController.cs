dcaa981015c2608e6ea9c44b9b003a3eb2e45c50
aebayram <a.e.bayram@hotmail.com>
2023-06-16 16:50:06 +0300

    prescriptioncontroller da değişiklikler yapıldı

--- ReceteX.Web/Controllers/PrescriptionController.cs
+++ ReceteX.Web/Controllers/PrescriptionController.cs
@@ -1,115 +1,128 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ReceteX.Models;
 using ReceteX.Repository.Shared.Abstract;
+using System.Security.Claims;
 
 namespace ReceteX.Web.Controllers
 {
-	[Authorize]
-	public class PrescriptionController : Controller
-	{
-		private readonly IUnitOfWork unitOfWork;
+    [Authorize]
+    public class PrescriptionController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
 
-		public PrescriptionController(IUnitOfWork unitOfWork)
-		{
-			this.unitOfWork = unitOfWork;
-		}
-
-		[HttpGet]
-		[Route("ReceteYaz/{id?}")]
-		public IActionResult Write(string id)
-		{
-			Prescription prescription = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id==Guid.Parse(id));
-
-			return View(prescription);
-		}
+        public PrescriptionController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
 
 
-		[HttpPost]
-		public IActionResult Create(Prescription prescription)
-		{
-			unitOfWork.Prescriptions.Add(prescription);
-			unitOfWork.Save();
-
-			return Json(prescription);
-		}
-
-		[HttpPost]
-		public IActionResult AddDiagnosis(Guid prescriptionId, Guid diagnosisId)
-		{
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-			Diagnosis asilDiagnosis = unitOfWork.Diagnoses.GetFirstOrDefault(d => d.Id==diagnosisId);
-
-			asil.Diagnoses.Add(asilDiagnosis);
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-
-			return Ok();
-
-		}
-
-		[HttpPost]
-		public IActionResult AddDescription(Guid prescriptionId, Description description)
-		{
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-			asil.Descriptions.Add(description);
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-			return Ok();
-		}
-
-		[HttpPost]
-		public IActionResult AddMedicine(Guid prescriptionId, PrescriptionMedicine prescriptionMedicine)
-		{
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-			asil.PrescriptionMedicines.Add(prescriptionMedicine);
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-			return Ok();
-
-		}
-
-		[HttpPost]
-		public IActionResult RemoveMedicine(Guid prescriptionId, Guid prescriptionMedicineId)
-		{
-
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-
-			asil.PrescriptionMedicines.Remove(asil.PrescriptionMedicines.FirstOrDefault(pm => pm.Id == prescriptionMedicineId));
-
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-			return Ok();
-
-		}
-
-		[HttpPost]
-		public IActionResult RemoveDescription(Guid prescriptionId, Guid descriptionId)
-		{
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-			asil.Descriptions.Remove(asil.Descriptions.FirstOrDefault(d => d.Id == descriptionId));
-
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-			return Ok();
-
-		}
-
-		[HttpPost]
-		public IActionResult RemoveDiagnosis(Guid prescriptionId, Guid diagnosisId)
-		{
-			Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
-			asil.Diagnoses.Remove(asil.Diagnoses.FirstOrDefault(d => d.Id == diagnosisId));
-
-			unitOfWork.Prescriptions.Update(asil);
-			unitOfWork.Save();
-			return Ok();
-
-		}
+        public IActionResult Index()
+        {
+            Prescription prescription = new Prescription();
+            prescription.StatusId = unitOfWork.Statuses.GetFirstOrDefault(s => s.Name == "Taslak").Id;
+            prescription.AppUserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            unitOfWork.Prescriptions.Add(prescription);
+            unitOfWork.Save();
+            return View(prescription);
 
 
+        }
 
-	}
+
+        [HttpPost]
+        public IActionResult Create(Prescription prescription)
+        {
+            unitOfWork.Prescriptions.Add(prescription);
+            unitOfWork.Save();
+
+            return Json(prescription);
+        }
+
+        [HttpPost]
+        public IActionResult AddDiagnosis(Guid prescriptionId, Guid diagnosisId)
+        {
+            Prescription asil = unitOfWork.Prescriptions.GetAll(p => p.Id == prescriptionId).Include(p => p.Diagnoses).First();
+            Diagnosis asilDiagnosis = unitOfWork.Diagnoses.GetFirstOrDefault(d => d.Id==diagnosisId);
+
+            asil.Diagnoses.Add(asilDiagnosis);
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult GetDiagnoses(Guid prescriptionId)
+        {
+            return Json(unitOfWork.Prescriptions.GetAll(p => p.Id == prescriptionId).Include(p => p.Diagnoses));
+
+        }
+
+        [HttpPost]
+        public IActionResult AddDescription(Guid prescriptionId, Description description)
+        {
+            Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
+            asil.Descriptions.Add(description);
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddMedicine(Guid prescriptionId, PrescriptionMedicine prescriptionMedicine)
+        {
+            Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
+            asil.PrescriptionMedicines.Add(prescriptionMedicine);
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult RemoveMedicine(Guid prescriptionId, Guid prescriptionMedicineId)
+        {
+
+            Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
+
+            asil.PrescriptionMedicines.Remove(asil.PrescriptionMedicines.FirstOrDefault(pm => pm.Id == prescriptionMedicineId));
+
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult RemoveDescription(Guid prescriptionId, Guid descriptionId)
+        {
+            Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
+            asil.Descriptions.Remove(asil.Descriptions.FirstOrDefault(d => d.Id == descriptionId));
+
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult RemoveDiagnosis(Guid prescriptionId, Guid diagnosisId)
+        {
+            Prescription asil = unitOfWork.Prescriptions.GetFirstOrDefault(p => p.Id == prescriptionId);
+            asil.Diagnoses.Remove(asil.Diagnoses.FirstOrDefault(d => d.Id == diagnosisId));
+
+            unitOfWork.Prescriptions.Update(asil);
+            unitOfWork.Save();
+            return Ok();
+
+        }
+
+
+
+    }
 }
 
 //GET: GET isteği, belirtilen bir kaynağın(örneğin bir web sayfası veya bir veri) alınmasını istemek için kullanılır. Bu yöntem, sunucudan veriyi almak için kullanılır ve genellikle bir kaynağın okunması veya görüntülenmesi için kullanılır. GET istekleri genellikle URL üzerinden parametrelerle iletilir ve verilerin sunucudan alınmasını sağlar.
