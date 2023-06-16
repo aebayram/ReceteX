22b0866160fcf1da77a050c132d0094113db5a96
aebayram <a.e.bayram@hotmail.com>
2023-06-16 20:21:37 +0300

    home ındex eklendi

--- ReceteX.Web/Controllers/MedicineController.cs
+++ ReceteX.Web/Controllers/MedicineController.cs
@@ -8,85 +8,77 @@ using System.Xml;
 
 namespace ReceteX.Web.Controllers
 {
-    public class MedicineController : Controller
-    {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly XmlRetriever xmlRetriever;
+	public class MedicineController : Controller
+	{
+		private readonly IUnitOfWork unitOfWork;
+		private readonly XmlRetriever xmlRetriever;
+		public MedicineController(IUnitOfWork unitOfWork, XmlRetriever xmlRetriever)
+		{
+			this.unitOfWork = unitOfWork;
+			this.xmlRetriever = xmlRetriever;
+		}
 
-        public MedicineController(IUnitOfWork unitOfWork, XmlRetriever xmlRetriever)
-        {
-            this.unitOfWork = unitOfWork;
-            this.xmlRetriever = xmlRetriever;
-        }
+		public async Task ParseAndSaveFromXml(string xmlContent)
+		{
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(xmlContent);
 
-        public async Task ParseAndSaveFromXml(string xmlContent)
-        {
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xmlContent);
-            XmlNodeList medicinesFromXml = xmlDoc.SelectNodes("/ilaclar/ilac");
-            //datatabse'deki tüm aktif nesneler
-            IQueryable<Medicine> medicinesFromDb = unitOfWork.Medicines.GetAll().AsNoTracking().OrderBy(m => m.Name).ToList().AsQueryable<Medicine>();
+			XmlNodeList medicinesFromXml = xmlDoc.SelectNodes("/ilaclar/ilac");//ilaclar altındaki her bir ilac boğumu için
+			IQueryable<Medicine> medicinesFromDb = unitOfWork.Medicines.GetAll().AsNoTracking().OrderBy(m => m.Name).ToList().AsQueryable<Medicine>(); // performans için tüm verileri çekmiyor. o yüzden sorguyu exec ettirip tekrar queryable yatık(ierisine linq sorgusu atabilmek için queriyable yatık)
 
-            //database'deki tüm silinmiş nesneler
-            IQueryable<Medicine> deletedMedicinesFromDb = unitOfWork.Medicines.GetAllDeleted().AsNoTracking().OrderBy(m => m.Name).ToList().AsQueryable<Medicine>();
+			//db de tüm silinmiş nesneleri getirir.
+			IQueryable<Medicine> deletedMedicinesFromDb = unitOfWork.Medicines.GetAllDeleted().AsNoTracking().OrderBy(m => m.Name).ToList().AsQueryable<Medicine>();
 
-            //Yeni kayıtları aktaran döngümüz.
-            foreach (XmlNode medicine in medicinesFromXml)
-            {
-                string barcodeFromXml = medicine.SelectSingleNode("barkod").InnerText;
+			foreach (XmlNode medicine in medicinesFromXml)
+			{
+				string barcodeFromXml = medicine.SelectSingleNode("barkod").InnerText;
+				if (!medicinesFromDb.Any(m => m.Barcode == barcodeFromXml))
+				{
+					Medicine med = new Medicine();
+					med.Name = medicine.SelectSingleNode("ad").InnerText;
+					med.Barcode = barcodeFromXml;
+					unitOfWork.Medicines.Add(med);
+				}
+				else
+				{
+					Medicine medSilinmis = deletedMedicinesFromDb.FirstOrDefault(m => m.Barcode == barcodeFromXml);
+					if (medSilinmis != null)
+					{
+						medSilinmis.isDeleted = false;
+						unitOfWork.Medicines.Update(medSilinmis);
+					}
+				}
+			}
+			unitOfWork.Save();
+			//xml de olmayanları db den silmek(isDeleted= true) için
+			IEnumerable<XmlNode> medicinesFromXmlEnumerable = xmlDoc.SelectNodes("/ilaclar/ilac").Cast<XmlNode>();
+			foreach (Medicine ilac in medicinesFromDb) // xml içerisine linq ile sorgu atabilmek için Ienumerable tipine cast ettik.
+			{
+				if (!medicinesFromXmlEnumerable.Any(x => x.SelectSingleNode("barkod").InnerText == ilac.Barcode))
+				{
+					ilac.isDeleted = true;
+					unitOfWork.Medicines.Update(ilac);
+				}
+			}
+			unitOfWork.Save();
 
-                if (!medicinesFromDb.Any(m => m.Barcode == barcodeFromXml))
-                {
-                    Medicine med = new Medicine();
-                    med.Name = medicine.SelectSingleNode("ad").InnerText;
-                    med.Barcode = barcodeFromXml;
-                    unitOfWork.Medicines.Add(med);
-                }
-                else
-                {
-                    Medicine medSilinmis = deletedMedicinesFromDb.FirstOrDefault(m => m.Barcode == barcodeFromXml);
-                    if (medSilinmis != null)
-                    {
-                        medSilinmis.isDeleted = false;
-                        unitOfWork.Medicines.Update(medSilinmis);
-                    }
-                }
-            }
+		}
 
-            unitOfWork.Save();
+		public async Task<IActionResult> UpdateMedicinesList()
+		{
+			string content = await xmlRetriever.GetXmlContect("https://www.ibys.com.tr/exe/ilaclar.xml");
+			await ParseAndSaveFromXml(content);
+			return RedirectToAction("Index");
+		}
 
-            //Kaynaktan silinmiş olan ilaçları databasede isdelete=true yapan döngümüz
-            IEnumerable<XmlNode> medicinesFromXmlEnumarable = xmlDoc.SelectNodes("/ilaclar/ilac").Cast<XmlNode>();
+		public IActionResult GetAll()
+		{
+			return Json(new { data = unitOfWork.Medicines.GetAll() });
+		}
 
-            foreach (Medicine ilac in medicinesFromDb)
-            {
-                if (!medicinesFromXmlEnumarable.Any(x => x.SelectSingleNode("barkod").InnerText==ilac.Barcode))
-                {
-                    ilac.isDeleted = true;
-                    unitOfWork.Medicines.Update(ilac);
-                }
-
-            }
-
-
-            unitOfWork.Save();
-
-        }
-
-        public async Task<IActionResult> UpdateMedicinesList()
-        {
-            string content = await xmlRetriever.GetXmlContect("https://www.ibys.com.tr/exe/ilaclar.xml");
-            await ParseAndSaveFromXml(content);
-
-
-
-            return RedirectToAction("Index");
-        }
-
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
